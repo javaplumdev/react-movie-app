f48c69c4b3345c37dcc6e5ad4cdab71a2c79e330
Charlito Mecarez Jr <70713746+javaplumdev@users.noreply.github.com>
2022-05-18 16:17:49 +0800

    update

--- movie-app/src/components/ShowMovie.jsx
+++ movie-app/src/components/ShowMovie.jsx
@@ -159,42 +159,51 @@ function ShowMovie() {
 			<div className="text-light">
 				<h5 className="mt-4">Movie reviews:</h5>
 				<div className="row">
-					{movieReviewState.slice(0, 3).map((item) => {
-						return (
-							<div className="col-md-4 bg-dark p-3 my-2">
-								<div className="d-flex align-items-center">
-									{item.author_details.avatar_path === null ? (
-										<img
-											src="https://media.istockphoto.com/vectors/male-user-icon-vector-id517998264?k=20&m=517998264&s=612x612&w=0&h=pdEwtkJlZsIoYBVeO2Bo4jJN6lxOuifgjaH8uMIaHTU="
-											style={{ height: '50px', width: '50px' }}
-											className="rounded-circle me-3"
-										/>
-									) : (
-										<img
-											src={item.author_details.avatar_path.substr(1)}
-											style={{ height: '50px', width: '50px' }}
-											className="rounded-circle me-3"
-										/>
-									)}
+					{movieReviewState.length > 1 ? (
+						<>
+							{movieReviewState.slice(0, 3).map((item) => {
+								return (
+									<div className="col-sm-4 bg-dark p-3 my-2">
+										<div className="d-flex align-items-center">
+											{item.author_details.avatar_path === null ||
+											!item.author_details.avatar_path.includes(
+												'/https://www.gravatar.com/avatar'
+											) ? (
+												<img
+													src="https://media.istockphoto.com/vectors/male-user-icon-vector-id517998264?k=20&m=517998264&s=612x612&w=0&h=pdEwtkJlZsIoYBVeO2Bo4jJN6lxOuifgjaH8uMIaHTU="
+													style={{ height: '50px', width: '50px' }}
+													className="rounded-circle me-3"
+												/>
+											) : (
+												<img
+													src={item.author_details.avatar_path.substr(1)}
+													style={{ height: '50px', width: '50px' }}
+													className="rounded-circle me-3"
+												/>
+											)}
 
-									<div>
-										<small className="lead fw-bold">
-											{item.author_details.username}
-										</small>
-										<br></br>
-										<small>{item.created_at}</small>
+											<div>
+												<small className="lead fw-bold">
+													{item.author_details.username}
+												</small>
+												<br></br>
+												<small>{item.created_at}</small>
+											</div>
+										</div>
+										<div className="mt-3">
+											{item.content.length > 200 ? (
+												<p>{item.content.substr(0, 200)}...</p>
+											) : (
+												<p>{item.content}</p>
+											)}
+										</div>
 									</div>
-								</div>
-								<div className="mt-3">
-									{item.content.length > 200 ? (
-										<p>{item.content.substr(0, 200)}...</p>
-									) : (
-										<p>{item.content}</p>
-									)}
-								</div>
-							</div>
-						);
-					})}
+								);
+							})}
+						</>
+					) : (
+						<p className="text-center lead">No movie reviews yet :( </p>
+					)}
 				</div>
 				<div className="text-end mt-3">
 					<p className="fw-bold">See more</p>
